6293f45d38950b673da2ee4ae902b398be2661ee
Yaz Khoury <yaz.khoury@gmail.com>
2022-11-23 19:50:54 -0500

    feat: add init.sh for cosmwasm, add README

---
+++ README.md
@@ -0,0 +1,4 @@
+# Devrel Tools
+
+Repo for developer toolings for devrel to reference
+in docs or for demo purposes.
---
+++ cosmwasm/init.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+VALIDATOR_NAME=validator1
+CHAIN_ID=celeswasm
+KEY_NAME=celeswasm-key
+TOKEN_AMOUNT="10000000000000000000000000uwasm"
+STAKING_AMOUNT=1000000000uwasm
+CHAINFLAG="--chain-id ${CHAIN_ID}"
+TXFLAG="--chain-id ${CHAIN_ID} --gas-prices 0uwasm --gas auto --gas-adjustment 1.3"
+NODEIP="--node http://127.0.0.1:26657"
+
+NAMESPACE_ID=$(echo $RANDOM | md5sum | head -c 16; echo;)
+echo $NAMESPACE_ID
+DA_BLOCK_HEIGHT=$(curl https://rpc.limani.celestia-devops.dev/block?height | jq -r '.result.block.header.height')
+echo $DA_BLOCK_HEIGHT
+
+rm -rf "$HOME"/.wasmd
+wasmd tendermint unsafe-reset-all
+wasmd init $VALIDATOR_NAME --chain-id $CHAIN_ID
+
+sed -i'' -e 's/^minimum-gas-prices *= .*/minimum-gas-prices = "0uwasm"/' "$HOME"/.wasmd/config/app.toml
+ed -i'' -e '/\[api\]/,+3 s/enable *= .*/enable = true/' "$HOME"/.wasmd/config/app.toml
+sed -i'' -e "s/^chain-id *= .*/chain-id = \"$CHAIN_ID\"/" "$HOME"/.wasmd/config/client.toml
+sed -i'' -e '/\[rpc\]/,+3 s/laddr *= .*/laddr = "tcp:\/\/0.0.0.0:26657"/' "$HOME"/.wasmd/config/config.toml
+sed -i'' -e 's/"time_iota_ms": "1000"/"time_iota_ms": "10"/' "$HOME"/.wasmd/config/genesis.json
+sed -i'' -e 's/bond_denom": ".*"/bond_denom": "uwasm"/' "$HOME"/.wasmd/config/genesis.json
+sed -i'' -e 's/mint_denom": ".*"/mint_denom": "uwasm"/' "$HOME"/.wasmd/config/genesis.json
+
+wasmd keys add $KEY_NAME --keyring-backend test
+wasmd add-genesis-account $KEY_NAME $TOKEN_AMOUNT --keyring-backend test
+wasmd gentx $KEY_NAME $STAKING_AMOUNT --chain-id $CHAIN_ID --keyring-backend test
+wasmd start --rollmint.aggregator true --rollmint.da_layer celestia --rollmint.da_config='{"base_url":"http://localhost:26659","timeout":60000000000,"gas_limit":6000000}' --rollmint.namespace_id $NAMESPACE_ID --rollmint.da_start_height $DA_BLOCK_HEIGHT
