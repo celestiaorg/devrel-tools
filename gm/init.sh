9792c2f62d536bc76feccfeff30f7e0d737ae5be
Samuel Laferriere <samlaf92@gmail.com>
2022-12-08 16:10:44 +0800

    Fixed errors in the scripts
    2 errors:
    1. gm's config folder is in ~/.gm and not ~/.gmd
    2. need to run collect-gentxs (see here: https://docs.cosmos.network/v0.46/run-node/run-node.html#adding-genesis-accounts) other start errors

--- gm/init.sh
+++ gm/init.sh
@@ -13,11 +13,12 @@ echo $NAMESPACE_ID
 DA_BLOCK_HEIGHT=$(curl https://rpc.limani.celestia-devops.dev/block | jq -r '.result.block.header.height')
 echo $DA_BLOCK_HEIGHT
 
-rm -rf "$HOME"/.gmd
+rm -rf "$HOME"/.gm
 gmd tendermint unsafe-reset-all
 gmd init $VALIDATOR_NAME --chain-id $CHAIN_ID
 
 gmd keys add $KEY_NAME --keyring-backend test
 gmd add-genesis-account $KEY_NAME $TOKEN_AMOUNT --keyring-backend test
 gmd gentx $KEY_NAME $STAKING_AMOUNT --chain-id $CHAIN_ID --keyring-backend test
+gmd collect-gentxs
 gmd start --rollmint.aggregator true --rollmint.da_layer celestia --rollmint.da_config='{"base_url":"http://localhost:26659","timeout":60000000000,"gas_limit":6000000}' --rollmint.namespace_id $NAMESPACE_ID --rollmint.da_start_height $DA_BLOCK_HEIGHT
