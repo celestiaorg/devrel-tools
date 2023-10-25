34775e16e44b3bb2f3b32d895556dae918536dc0
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2023-10-25 15:21:58 -0400

    feat: add celestia-app install scripts

--- celestia-app/install-temp.sh
+++ celestia-app/install-temp.sh
@@ -1,14 +1,26 @@
+#!/bin/bash
+
+# Create a log file to capture detailed logs
+LOGFILE="$HOME/celestia-temp/logfile.log"
+mkdir -p "$HOME/celestia-temp"
+touch "$LOGFILE"
+
+# Log and print the log file location
+echo "Log file is located at: $LOGFILE" | tee -a "$LOGFILE"
+
 # Create a temporary directory to work from
 TEMP_DIR="$HOME/celestia-temp"
 mkdir -p "$TEMP_DIR"
 cd "$TEMP_DIR"
 
-echo "Working from temporary directory: $TEMP_DIR"
+# Log and print a message
+echo "Working from temporary directory: $TEMP_DIR" | tee -a "$LOGFILE"
 
-# Set the latest compatible tag
+# Fetch the latest release tag from GitHub
 VERSION=v1.1.0
 
-echo "Latest version detected: $VERSION"
+# Log and print a message
+echo "Latest version detected: $VERSION" | tee -a "$LOGFILE"
 
 # Detect the operating system and architecture
 OS=$(uname -s)
@@ -19,28 +31,21 @@ case $ARCH in
     x86_64)
         ARCH="x86_64"
         ;;
-    aarch64)
-        ARCH="arm64"
-        ;;
-    arm64)
+    aarch64|arm64)
         ARCH="arm64"
         ;;
     *)
-        echo "Unsupported architecture."
+        echo "Unsupported architecture." | tee -a "$LOGFILE"
         exit 1
         ;;
 esac
 
 # Translate OS to expected format
 case $OS in
-    Linux)
-        OS="Linux"
-        ;;
-    Darwin)
-        OS="Darwin"
+    Linux|Darwin)
         ;;
     *)
-        echo "Unsupported operating system."
+        echo "Unsupported operating system." | tee -a "$LOGFILE"
         exit 1
         ;;
 esac
@@ -49,30 +54,57 @@ esac
 PLATFORM="${OS}_${ARCH}"
 URL="https://github.com/celestiaorg/celestia-app/releases/download/$VERSION/celestia-app_$PLATFORM.tar.gz"
 
-echo "Downloading from: $URL"
+# Log and print a message
+echo "Downloading from: $URL" | tee -a "$LOGFILE"
 
 # Download the tarball
-if ! wget "$URL"; then
-    echo "Download failed. Exiting."
+if ! wget "$URL" >> "$LOGFILE" 2>&1; then
+    echo "Download failed. Exiting." | tee -a "$LOGFILE"
     exit 1
+fi
+
+# Detect if running on macOS and use appropriate command for checksum
+if [ "$OS" = "Darwin" ]; then
+    CALCULATED_CHECKSUM=$(shasum -a 256 "celestia-app_$PLATFORM.tar.gz" | awk '{print $1}')
+else
+    CALCULATED_CHECKSUM=$(sha256sum "celestia-app_$PLATFORM.tar.gz" | awk '{print $1}')
+fi
+
+# Download checksums.txt
+if ! wget "https://github.com/celestiaorg/celestia-app/releases/download/$VERSION/checksums.txt" >> "$LOGFILE" 2>&1; then
+    echo "Failed to download checksums. Exiting." | tee -a "$LOGFILE"
+    exit 1
+fi
+
+# Find the expected checksum in checksums.txt
+EXPECTED_CHECKSUM=$(grep "celestia-app_$PLATFORM.tar.gz" checksums.txt | awk '{print $1}')
+
+# Verify the checksum
+if [ "$CALCULATED_CHECKSUM" != "$EXPECTED_CHECKSUM" ]; then
+    echo "Checksum verification failed. Exiting." | tee -a "$LOGFILE"
+    exit 1
+else
+    echo "Checksum verification successful." | tee -a "$LOGFILE"
 fi
 
 # Extract the tarball to the temporary directory
-if ! tar -xzf "celestia-app_$PLATFORM.tar.gz"; then
-    echo "Extraction failed. Exiting."
+if ! tar -xzf "celestia-app_$PLATFORM.tar.gz" >> "$LOGFILE" 2>&1; then
+    echo "Extraction failed. Exiting." | tee -a "$LOGFILE"
     exit 1
 fi
 
-echo "Binary extracted to: $TEMP_DIR"
+# Log and print a message
+echo "Binary extracted to: $TEMP_DIR" | tee -a "$LOGFILE"
 
 # Remove the tarball to clean up
 rm "celestia-app_$PLATFORM.tar.gz"
 
-echo "Temporary files cleaned up."
+# Log and print a message
+echo "Temporary files cleaned up." | tee -a "$LOGFILE"
 
 # Provide final instructions to the user
-echo "You can navigate to $TEMP_DIR to find and run the Celestia app."
-echo "To run the app and check its version, you may execute the following commands:"
-echo "  cd $TEMP_DIR"
-echo "  chmod +x celestia-appd"
-echo "  ./celestia-appd version"
+echo "You can navigate to $TEMP_DIR to find and run the Celestia app." | tee -a "$LOGFILE"
+echo "To run the app and check its version, you may execute the following commands:" | tee -a "$LOGFILE"
+echo "  cd $TEMP_DIR" | tee -a "$LOGFILE"
+echo "  chmod +x celestia-appd" | tee -a "$LOGFILE"
+echo "  ./celestia-appd version" | tee -a "$LOGFILE"
